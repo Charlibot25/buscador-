b065d17c541195eadfc8873839785d4372c2322c
rodorz <rodolfourinzar2008@gmail.com>
2026-09-17 13:14:57 -0600

    Orden del script corregido

--- buscar.sh
+++ buscar.sh
@@ -1,22 +1,24 @@
-#!/bin/batch
+#!/bin/bash
 
-resultado=$(grep -rl "home" "$1")
-if [ -z "$resultado" ]; then 
-	echo "No se encontraron resultados "
-else
-	echo "$resultado"
-fi
 
 if [ -z "$1" ]; then
     echo "Uso: $0 <carpeta>"
     exit 1
 fi
 
+
 if [ ! -d "$1" ]; then
     echo "Error: '$1' no es una carpeta"
     exit 1
 fi
 
-
 carpeta=$1
-grep -rl home $carpeta
+resultado=$(grep -rl home $carpeta)
+
+
+
+if [ -z "$resultado" ]; then
+    echo "No se encontraron resultados"
+else
+    echo "$resultado"
+fi
